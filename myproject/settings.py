66ce8c7990311dcfe694650a81fbdd266ef35c5c
polymathdanny <87679694+polymathdanny@users.noreply.github.com>
2022-04-13 17:13:32 -0400

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = 'aay0j_9b&ky3a7(8m8il+-1ud(scw12@w5!+5-=gsk6ynzi0ls'
 DEBUG = True
 
 # <HINT> add your cloud host here
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['polymathdanny.mybluemix.net','127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
@@ -124,4 +123,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
 MEDIA_URL = '/media/'
+
 
